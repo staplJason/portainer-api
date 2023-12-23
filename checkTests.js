ec361fb189ceee9a64ea15f4c03b00645e6aa02e
Jason Lake <jason.lake@staplcotn.com>
2023-12-23 15:38:17 -0600

    ci: Fix lint

--- checkTests.js
+++ checkTests.js
@@ -1,21 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const testResultsPath = path.join(__dirname, 'testResults.json');
+const testResultsPath = path.join(__dirname, 'testResults.json')
 
 fs.readFile(testResultsPath, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading test results:', err);
-        process.exit(1);
-    }
+  if (err) {
+    console.error('Error reading test results:', err)
+    process.exit(1)
+  }
 
-    const results = JSON.parse(data);
+  const results = JSON.parse(data)
 
-    if (results.numFailedTests === 0) {
-        console.log('All tests passed.');
-        process.exit(0); // Success
-    } else {
-        console.error(`There are test failures. Failed tests count: ${results.numFailedTests}`);
-        process.exit(1); // Failure
-    }
-});
+  if (results.numFailedTests === 0) {
+    console.log('All tests passed.')
+    process.exit(0) // Success
+  } else {
+    console.error(
+      `There are test failures. Failed tests count: ${results.numFailedTests}`
+    )
+    process.exit(1) // Failure
+  }
+})
